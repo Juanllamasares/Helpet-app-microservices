0a8ddf366e3014a4e541f9cc93a2c746affcc53e
Juan Llamasares <Juanllama160@gmail.com>
2023-02-05 13:02:46 -0300

    debug port

--- comment-service/src/main/java/com/helpet/commentservice/clients/PostFeingClient.java
+++ comment-service/src/main/java/com/helpet/commentservice/clients/PostFeingClient.java
@@ -4,10 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "post-service", url = "http://localhost:8081")
-@RequestMapping("/api/posts")
+@FeignClient(name = "post-service", url = "http://localhost:8081/api/posts")
 public interface PostFeingClient{
     
     @GetMapping("/{postId}")
